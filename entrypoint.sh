deb13aa827f466bd2ce01286e3548fdc73452065
marqujlm <joao.marques@codepy.com.br>
2026-03-17 16:59:07 -0300

    fix: variables DIFY and Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,11 @@ find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|VITE_FIREBASE_AP
 find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|VITE_API_DIFY_PLACEHOLDER|${VITE_API_DIFY}|g" {} +
 find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|VITE_ENDPOINT_DIFY_PLACEHOLDER|${VITE_ENDPOINT_DIFY}|g" {} +
 
+# O Railway exige que a aplicação escute na porta que ele define via variável ambiente $PORT
+PORT="${PORT:-80}"
+echo "Configurando Nginx para escutar na porta ${PORT}..."
+sed -i "s|PORT_PLACEHOLDER|${PORT}|g" /etc/nginx/conf.d/nginx.conf
+
 echo "Variaveis injetadas. Iniciando Nginx..."
 
 # Executa o Nginx (o "$@" repassa os comandos recebidos no Dockerfile)
