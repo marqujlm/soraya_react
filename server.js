6313fa84e7127f996cf8c6f49ea52bb1b03d8919
marqujlm <joao.marques@codepy.com.br>
2026-03-19 00:57:34 -0300

    fix: container error

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/config.js", (req, res) => {
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Todas as outras rotas (React Router SPA) devolvem o index.html
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
